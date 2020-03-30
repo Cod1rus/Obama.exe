7ec9c96a027d3a10089a8021991f5f4519a12bc8
Cod1rus <nils@gaion.de>
2020-03-30 12:03:58 +0200

    Basic Charakter Controllwe finished

--- Obama.exe/Assets/Scripts/PlayerController.cs
+++ Obama.exe/Assets/Scripts/PlayerController.cs
@@ -6,14 +6,17 @@ public class PlayerController : MonoBehaviour
 {
     public CharacterController controller;
     public Transform GroundCheck;
+   // public Transform AutschCheck;
 
     public float jumpHeight = 3f;
     public float speed = 12f;
     public float gravity = -9.81f;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
+   // public LayerMask DeckeMask;
 
     private bool isGrounded;
+    //private bool isanDecke;
 
 
     Vector3 velocity;
@@ -21,11 +24,14 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         isGrounded = Physics.CheckSphere(GroundCheck.position, groundDistance, groundMask);
+       // isanDecke = Physics.CheckSphere(AutschCheck.position, groundDistance, DeckeMask);
 
-        if (isGrounded && velocity.y < 0)
-        {
+        if (isGrounded && velocity.y < 0){
             velocity.y = -2f;
         }
+
+
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
@@ -33,10 +39,14 @@ public class PlayerController : MonoBehaviour
 
         controller.Move(move * speed * Time.deltaTime);
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
+        if (Input.GetButtonDown("Jump") && isGrounded){
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
+
+       // if (isanDecke)
+       //{
+       //     velocity.y = -2f;
+       // }
 
         velocity.y += gravity * Time.deltaTime;
 
