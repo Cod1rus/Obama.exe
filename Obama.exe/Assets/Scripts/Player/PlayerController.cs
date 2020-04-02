8bcf090f413cca10a6203349c0109ef5e48bb2ab
Cod1rus <nils@gaion.de>
2020-04-02 17:06:09 +0200

    new Player Controller & startet with Networking
    new Rigdidbody Player Controller (only basics)
     implemented Basic Networking layer for Connecting Players

---
+++ Obama.exe/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed = 10f;
+    [SerializeField]
+    private float MouseSensitivity = 200;
+    [SerializeField]
+    private float JumpHeight = 4;
+
+    private PlayerMotor Motor;
+
+    private void Start()
+    {
+        Motor = GetComponent<PlayerMotor>();
+
+    }
+
+    private void Update()
+    {
+        float _xMove = Input.GetAxisRaw("Horizontal");
+        float _zMove = Input.GetAxisRaw("Vertical");
+        float _jumpMove = Input.GetAxisRaw("Jump");
+
+
+        Vector3 _movHorizontal = transform.right * _xMove;
+        Vector3 _movVertical = transform.forward * _zMove;
+        Vector3 _movJump = new Vector3(0f, _jumpMove * JumpHeight, 0f);
+
+        Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
+        _velocity = (_velocity + _movJump);
+
+        Motor.Move(_velocity);
+
+        float _yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0f, _yRot, 0f) * MouseSensitivity;
+
+        Motor.Rotate(_rotation);
+
+
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+
+
+        Vector3 _cameraRotation = new Vector3(_xRot, 0f, 0f) * MouseSensitivity;
+
+        Motor.RotateCamera(_cameraRotation);
+
+
+
+    }
+}
