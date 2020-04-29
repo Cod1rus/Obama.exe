492f06b25a72797847e96edab4bc3b94db56bdc7
Cod1rus <nils@gaion.de>
2020-04-29 13:22:13 +0200

    Started Scetching out a Main Menu

---
+++ Obama.exe/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mainmenu;
+
+    public void PlayGame()
+    {
+        Debug.Log("Starting Game");
+        mainmenu.SetActive(false);
+    }
+}
