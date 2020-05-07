aca78d4e425dd095172e82f289f1ffe618bec8aa
Cod1rus <nils@gaion.de>
2020-05-07 13:45:50 +0200

    Just a quick random commit

--- Obama.exe/Assets/Scripts/Weapon/PlayerWeapon.cs
+++ Obama.exe/Assets/Scripts/Weapon/PlayerWeapon.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 [System.Serializable]
 public class PlayerWeapon
 {
-    public string name;
+    public string name = "Default";
 
     public int damage = 10;
+
     public float range = 200f;
+    public float recoil = 0f;
+    public float fireRate = 10f;
+
+    public bool isAutomatic = false;
 
 }
