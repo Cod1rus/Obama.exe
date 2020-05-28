e8c962d47f6ae6b3b9ec596c9147419580d7163a
Cod1rus <nils@gaion.de>
2020-05-28 21:13:36 +0200

    Started working on a MapManagerClass
    by far not finished, but setuped the ground layout for the MapManager

---
+++ Obama.exe/Assets/MapManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] mapList;
+    [SerializeField]
+    private Transform lobbyMap;
+    [SerializeField]
+    private Transform overtimeMap;
+
+
+    private void Start()
+    {
+        activeMap = lobbyMap;
+    }
+
+    private Transform activeMap;
+    void ChangeMapTo(string _mapname)
+    {
+        clearActiveMap();
+        //TODO: Map changing through instanciating and replacing the active Map prefap
+
+        Debug.Log("Map Changed to: ");
+    }
+    void ChangeMapTo(Transform _map) //just for changing the map via a Transform pointer
+    {       
+        ChangeMapTo(_map.name);
+    }
+
+    void clearActiveMap()
+    {
+        //TODO: Clearing out the active Map prefab
+        Debug.Log("Active Map cleared out!");
+    }
+
+    void GoToOvertime()
+    {
+        ChangeMapTo(overtimeMap);
+    }
+}
