9295fe4471f2b16aa7ad0b12a0c2cf527a42b4bc
Zwackelmann173 <57451356+Zwackelmann173@users.noreply.github.com>
2020-06-16 20:37:43 +0200

    More DummyOptions stuff

---
+++ Obama.exe/Assets/GUI/HitdetectShaker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitdetectShaker : MonoBehaviour
+{
+    [SerializeField]
+    private bool shakeObject;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D rayHit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, Mathf.Infinity);
+
+            if (rayHit.collider != null)
+            {
+                Debug.Log("objectHit");
+                if (shakeObject)
+                {
+                    ShakeThisObject(rayHit.collider.gameObject);
+                }
+            }
+        }
+    }
+
+    private void ShakeThisObject(GameObject shakeObject)
+    {
+        shakeObject.GetComponent<Shaker>().ShakeMe();
+    }
+}
---
+++ Obama.exe/Assets/GUI/Shaker.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shaker : MonoBehaviour
+{
+    private bool shaking = false;
+
+    [SerializeField]
+    private float shakeAmt;
+
+    private void Update()
+    {
+        if(shaking)
+        {
+            Vector3 newPos = Random.insideUnitSphere * (Time.deltaTime * shakeAmt);
+            newPos.y = transform.position.y;
+            newPos.x = transform.position.x;
+
+            transform.position = newPos;
+        }
+    }
+
+    public void ShakeMe()
+    {
+        StartCoroutine("ShakeNow");
+    }
+
+    IEnumerator ShakeNow()
+    {
+        Vector3 originalPos = transform.position;
+
+        if (shaking == false)
+        {
+            shaking = true;
+        }
+
+        yield return new WaitForSeconds(0.25f);
+
+        shaking = false;
+        transform.position = originalPos;
+    }
+}
