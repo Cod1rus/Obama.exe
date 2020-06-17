5be910746da0648a5058299bfe4af7ed949cdd8b
Zwackelmann173 <57451356+Zwackelmann173@users.noreply.github.com>
2020-06-18 00:13:08 +0200

    Lobby closed up
    Weather forecast: Flying memes in the sky :airplane:

---
+++ Obama.exe/Assets/CustomStuff/Rotator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField]
+    private bool X_Achse;
+    [SerializeField]
+    private bool Y_Achse;
+    [SerializeField]
+    private bool Z_Achse;
+
+    public float m_drehsbeve = 10.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (X_Achse)
+        {
+            this.transform.rotation *= Quaternion.Euler(Time.deltaTime * m_drehsbeve, 0, 0);
+        }
+
+        if (Y_Achse)
+        {
+            this.transform.rotation *= Quaternion.Euler(0, Time.deltaTime * m_drehsbeve, 0);
+        }
+
+        if (Z_Achse)
+        {
+            this.transform.rotation *= Quaternion.Euler(0, 0, Time.deltaTime * m_drehsbeve);
+        }
+        
+    }
+}
