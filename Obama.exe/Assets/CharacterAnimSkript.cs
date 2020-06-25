16ce176d17ed41f6c772959cd653665195fdfb7d
Zwackelmann173 <57451356+Zwackelmann173@users.noreply.github.com>
2020-06-25 23:50:55 +0200

    FSM fertig Animationen werden aber nicht wieder gegeben
    nix tun == Idle
    W == Walking
    T == TPose

--- Obama.exe/Assets/CharacterAnimSkript.cs
+++ Obama.exe/Assets/CharacterAnimSkript.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class CharacterAnimSkript : MonoBehaviour
 {
+    private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            anim.CrossFade("MikeTPose", 0.1f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.CrossFade("MikeWalking", 0.1f);
+        }
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            anim.SetBool("IsWalking", true);
+            anim.SetBool("IsIdle", false);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isIdle", true);
+        }
     }
 }
